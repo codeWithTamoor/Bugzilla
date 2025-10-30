c3bc9aca2561a223166fd28fbc0d525c2107efd8
codeWithTamoor <workwithtamoor@gmail.com>
2025-10-30 17:10:24 +0500

    rspec test User model qa.rb

--- spec/models/qa_spec.rb
+++ spec/models/qa_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Qa, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it {should have_many(:reported_tickets)}
+  it {should belong_to(:manager).optional}
 end
